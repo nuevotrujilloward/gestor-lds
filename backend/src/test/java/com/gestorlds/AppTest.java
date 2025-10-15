04350f6567e4806758bee193417559bf730f9a27
nuevotrujilloward <nuevotrujilloward@gmail.com>
2025-10-15 17:48:20 -0500

    v 0.1

---
+++ backend/src/test/java/com/gestorlds/AppTest.java
@@ -0,0 +1,15 @@
+package com.gestorlds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest
+class GestorLdsApplicationTest {
+
+    @Test
+    void contextLoads() {
+        assertTrue(true);
+    }
+}
